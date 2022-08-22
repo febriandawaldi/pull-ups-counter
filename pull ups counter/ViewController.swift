42faa26a307313d08a4679d94696fef3ff7669c2
waldifebrianda <102569233+ioswaldi@users.noreply.github.com>
2022-08-22 15:34:11 +0700

    Initial Commit

---
+++ pull ups counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pull ups counter
+//
+//  Created by Waldi Febrianda on 22/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        BackgroundColo
+    }
+
+
+}
+
